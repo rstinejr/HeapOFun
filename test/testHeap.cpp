49a1d9b7c101464def0c47f636ce7a4ec425b1e5
rstinejr <bob@waltonstine.net>
2016-10-02 09:57:55 -0400

    Test add and top.

--- test/testHeap.cpp
+++ test/testHeap.cpp
@@ -8,12 +8,11 @@ using namespace hfun;
 struct Ele
 {
     const int  key;
-    const char val;
+    char const * val;
 
-    Ele(int k, char v)
+    Ele(int k, char const * v)
       : key(k)
       , val(v)
-      
     {
     }
 
@@ -37,8 +36,36 @@ BOOST_AUTO_TEST_CASE(append)
 {
     Heap<Ele> h;
     BOOST_CHECK_EQUAL(0, h.size());
-    Ele *e = new Ele(123, 'a');
+    Ele *e = new Ele(123, "a");
     h.add(e);
     BOOST_CHECK_EQUAL(1, h.size());
     delete e;
 }
+
+BOOST_AUTO_TEST_CASE(top)
+{
+    Heap<Ele> h;
+
+    Ele *one   = new Ele(1, "one");
+    Ele *two   = new Ele(2, "two");
+    Ele *three = new Ele(3, "three");
+    Ele *zero  = new Ele(0, "zero");
+
+    h.add(one);
+    BOOST_CHECK_EQUAL(1, h.top()->key);
+    
+    h.add(two);
+    BOOST_CHECK_EQUAL(2, h.top()->key);
+
+    h.add(three);
+    BOOST_CHECK_EQUAL(3, h.top()->key);
+
+    h.add(zero);
+    BOOST_CHECK_EQUAL(3, h.top()->key);
+    
+    delete one;
+    delete two;
+    delete three;
+    delete zero;
+}
+
