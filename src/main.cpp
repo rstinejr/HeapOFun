b4c7acf9050172c8dbb98f8e42edb2e0c26d08d2
R.H. Stine, Jr <bob.stine@neustar.biz>
2016-10-03 09:14:43 -0400

    Add assertions, test code to run on systems that don't have Boost test framework installed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Heap.h>
 
+#include <assert.h>
+
 #include <iostream>
 
 using namespace std;
@@ -7,8 +9,14 @@ using namespace hfun;
 
 struct Node
 {
-    int  key;
-    char val;
+    const int  key;
+    char const *val;
+
+    Node(int k, char const *v)
+      : key(k)
+      , val(v)
+    {
+    }
 
     bool operator >(Node& other)
     {
@@ -17,10 +25,53 @@ struct Node
 
 };
 
+void testPop()
+{
+        Heap<Node> h;
+
+        Node *eles[] =
+            { new Node(1, "one"),  new Node(2, "two"),  new Node(4, "four"),
+              new Node(3, "three"), new Node(0, "zero"), new Node(12, "twelve")};
+
+        for (uint32_t ii = 0; ii < sizeof(eles)/sizeof(eles[0]); ii++)
+        {
+            h.insert(eles[ii]);
+        }
+
+        Node *e;
+
+        e  = h.pop();
+        assert(12 == e->key);
+        delete e;
+
+        e = h.pop();
+        assert(4 == e->key);
+        delete e;
+
+        e = h.pop();
+        assert(3 == e->key);
+        delete e;
+
+        e = h.pop();
+        assert(2 == e->key);
+        delete e;
+
+        e = h.pop();
+        assert(1 == e->key);
+        delete e;
+
+        e = h. pop();
+        assert(0 == e->key);
+        delete e;
+}
+
 int main(int argc, char *argv[])
 {
     Heap<Node> heap;
 
+    testPop();
+
     cout << "Done." << endl;
+
     return 0;
 }    
